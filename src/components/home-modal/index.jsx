e7c7d0dae13eb0aafdd20f8f5c0db6a1e0fe225b
qAbdullajon <azikmelor7705@gmail.com>
2024-12-09 21:07:04 +0500

    qwqwoas

--- src/components/home-modal/index.jsx
+++ src/components/home-modal/index.jsx
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
-import { Button, Form, Input, Modal } from "antd";
+import { Button, Form, Input, Modal, message } from "antd";
 const HomeModal = ({ open, onCancel }) => {
   const [form] = Form.useForm();
-  const onSubmit = async (value) => {
-    console.log(value);
-    fetch("https://script.google.com/macros/s/AKfycbxyt0hoRIvrcpcH10L9idl3Ylr8KyyknngQIHu8QAB5wgYmE6efcOg6AKIGps8acNAW/exec", {
-      method: "POST",
-      body: { ...value, Data: "12.03.2024" },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        alert(data.msg);
-      })
-      .catch((err) => {
-        console.error("There was a problem with the fetch operation:", err);
-        alert("Submission failed. Please try again.");
+  const [loading, setLoading] = useState(false);
+  const onSubmit = async (values) => {
+    const { name, phone } = values;
+    setLoading(true);
+
+    try {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbz4Jbe_Va4_wVS8HtsE0xZwpM8on9nDbwZsyxGPVAHT97VTw6AfRMAYpvSZfLQfC-mhOg/exec", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, phone }),
       });
+
+      if (response.ok) {
+        alert("/obuna");
+      } else {
+        message.error("Ma'lumot yuborishda xato.");
+      }
+    } catch (error) {
+      message.error("Xato: " + error.message);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <>
       <Modal open={open} footer={null} onCancel={onCancel}>
@@ -31,13 +36,13 @@ const HomeModal = ({ open, onCancel }) => {
         </p>
         <Form onFinish={onSubmit} form={form} layout="vertical">
           <Form.Item name="Name" label="Ismingiz" rules={[{ required: true, message: "Iltimos, ismingizni kiriting!" }]}>
-            <Input className="py-3" size="large" placeholder="Name" />
+            <Input className="py-3" size="large" placeholder="name" />
           </Form.Item>
-          <Form.Item name="Phone" label="Telefon raqamingiz" rules={[{ required: true, message: "Iltimos, telifon reqamingizni kiriting!" }]}>
+          <Form.Item name="phone" label="Telefon raqamingiz" rules={[{ required: true, message: "Iltimos, telifon reqamingizni kiriting!" }]}>
             <Input className="py-3" size="large" placeholder="Phone number" />
           </Form.Item>
           <Form.Item>
-            <Button htmlType="submit" className="submit-btn w-full uppercase mt-5 font-bold" size="large">
+            <Button loading={loading} htmlType="submit" className="submit-btn w-full uppercase mt-5 font-bold" size="large">
               Davom etish
             </Button>
           </Form.Item>
